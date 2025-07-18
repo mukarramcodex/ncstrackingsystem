a231a7752a464c6ac02add4ca902389f2ffd4ffa
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-18 15:50:32 +0500

    component - isssue

--- app/View/Components/financial-statement-card.php
+++ app/View/Components/financial-statement-card.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class financial-statement-card extends Component
+class financialstatementcard extends Component
 {
     /**
      * Create a new component instance.
