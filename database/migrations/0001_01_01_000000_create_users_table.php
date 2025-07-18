ea920e791af096e29ddb7b896d74e8048ca5cbfb
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-18 16:11:12 +0500

    adding role into db

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['admin','staff']);
+            $table->enum('role', ['superadmin','admin','staff']);
             $table->rememberToken();
             $table->timestamps();
         });
