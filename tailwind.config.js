b8cbf7e43b2b76cad1eb6bab2e305d23b7723908
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-24 13:47:23 +0500

    Style changings

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,22 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                primary: '#1a365d',
+                secondary: '#ed8936'
+            },
+            borderRadius: {
+                'none': '0px',
+                'sm': '4px',
+                DEFAULT: '8px',
+                'md': '12px',
+                'lg': '16px',
+                'xl': '20px',
+                '2xl': '24px',
+                '3xl': '32px',
+                'full': '9999px',
+                'button': '8px'
+            }
         },
     },
 
