d405ca71363d54f8ddd0955d893e4338beddc964
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-25 23:08:40 +0500

    Issue Solved

--- resources/views/parcels/index.blade.php
+++ resources/views/parcels/index.blade.php
@@ -3,9 +3,8 @@
             <h1 class="text-2xl font-bold mb-6">
                 All Parcels
             </h1>
-            
+
             <a href="{{ route('parcels.create') }}" class="mb-4 inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Create Parcels</a>
-            <a href="{{ route('parcels.form') }}" class="mb-4 inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Create Shipment</a>
             <div class="bg-white shadow rounded-lg overflow-hidden">
                 <table class="min-w-full table-auto">
                 <thead class="bg-gray-100">
