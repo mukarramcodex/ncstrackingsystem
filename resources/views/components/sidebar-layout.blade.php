457735bda776dc07b847554ba8d9686c480cf526
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-26 00:44:34 +0500

    sidebar-layout changings

--- resources/views/components/sidebar-layout.blade.php
+++ resources/views/components/sidebar-layout.blade.php
@@ -1,3 +1,10 @@
-<div>
-    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
+<div class="flex h-screen bg-gray-100">
+    <div class="sidebar bg-gray-800 text-white w-64 min-h-screen fixed transition-all duration-300 ease-in-out">
+        @include('layouts.sidebar')
+    </div>
+    <div class="flex-1 flex flex-col overflow-hidden ml-64">
+        <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 p-4">
+            {{ $slot }}
+        </main>
+    </div>
 </div>
