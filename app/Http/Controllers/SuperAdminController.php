82d79b4a5e181baf40a5caebe442dcd64f0905e8
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-25 22:37:33 +0500

    Update SuperAdminController.php

--- app/Http/Controllers/SuperAdminController.php
+++ app/Http/Controllers/SuperAdminController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class SuperAdminController extends Controller
 {
-    //
+    public function index()
+    {
+        //
+    }
+
+    public function show()
+    {
+        return view('SuperAdmin.dashboard');
+    }
+
+    public function store()
+    {
+        //
+    }
 }
