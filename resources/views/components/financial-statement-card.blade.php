fc37aecc3d09a149baa1dfdc7226346c6d934284
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-25 22:50:33 +0500

    Update financial-statement-card.blade.php

--- resources/views/components/financial-statement-card.blade.php
+++ resources/views/components/financial-statement-card.blade.php
@@ -3,25 +3,27 @@
     'income' => 0,
     'expense' => 0,
     'balance' => null,
-    'currency' => 'PKR',
+    'currency' => 'PKR'
 ])
 
 <div class="bg-white rounded-lg shadow-md overflow-hidden">
-    <div class="bg-gray-800 text-white px-4 py-2 text-center font-bold">
+    <div class="bg-blue-800 text-white px-4 py-2 text-center font-bold">
         {{ $title }}
     </div>
     <ul class="divide-y divide-gray-200">
         <li class="px-4 py-3 flex justify-between">
             <span class="font-medium">Income</span>
-            <span>{{ $currency }}{{ number_format($icome, 2) }}</span>
+            <span>PKR 0</span>
         </li>
         <li class="px-4 py-3 flex justify-between">
             <span class="font-medium">Expense</span>
-            <span>{{ $currency }}{{ number_format($expense, 2) }}</span>
+            <span>PKR 0</span>
         </li>
-        <li class="px-4 py-3 flex justify-between">
+        <li class="px-4 py-3 flex justify-between bg-gray-50">
             <span class="font-medium">Balance</span>
-            <span>{{ $currency }}{{ number_format($balance ?? ($icome - $expense), 2) }}</span>
+            <span class="font-bold">
+                PKR 0
+            </span>
         </li>
     </ul>
 </div>
