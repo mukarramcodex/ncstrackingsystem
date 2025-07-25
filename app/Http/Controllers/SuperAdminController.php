9773e8e565a0965e157560bb8644311c647bba14
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-25 22:41:21 +0500

    Update SuperAdminController.php

--- app/Http/Controllers/SuperAdminController.php
+++ app/Http/Controllers/SuperAdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Parcel;
 use Illuminate\Http\Request;
 
 class SuperAdminController extends Controller
@@ -13,7 +14,15 @@ class SuperAdminController extends Controller
 
     public function show()
     {
-        return view('SuperAdmin.dashboard');
+        $totalParcels = Parcel::count();
+        $deliveredParcels = Parcel::where('status', 'Delivered')->count();
+        $inTransitParcels = Parcel::where('status', 'In Transit')->count();
+
+        return view('SuperAdmin.dashboard', [
+            'totalParcels' => $totalParcels,
+            'deliveredParcels' => $deliveredParcels,
+            'inTransitParcels' => $inTransitParcels,
+        ]);
     }
 
     public function store()
