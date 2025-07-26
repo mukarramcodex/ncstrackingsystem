6ec648e06f636028e0a0d5f595a0254447eb68ec
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-26 11:22:31 +0500

    monir

--- app/View/Components/sidebar-layout.php
+++ app/View/Components/sidebar-layout.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class sidebar-layout extends Component
+class sidebarlayout extends Component
 {
     /**
      * Create a new component instance.
