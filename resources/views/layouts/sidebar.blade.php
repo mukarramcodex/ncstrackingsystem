1b9d57aba7e898323ddab4238732fdd8d65cac7b
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-27 16:18:54 +0500

    Update sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -95,40 +95,30 @@
 
             <li>
                 <a href="#" class="flex items-center p-2 rounded hover:bg-gray-700 ">
-                    <i class="ri-route-line"></i>
+                    <i class="ri-route-line w-5 h-5 mr-3"></i>
                     <span>Tracking Logs</span>
                 </a>
             </li>
 
             <li>
                 <a href="#" class="flex items-center p-2 rounded hover:bg-gray-700 ">
-                    <i class="ri-bar-chart-line"></i>
+                    <i class="ri-bar-chart-line w-5 h-5 mr-3"></i>
                     <span>Reports</span>
                 </a>
             </li>
 
+            <li>
+                <a href="#" class="flex items-center p-2 rounded hover:bg-gray-700 ">
+                    <i class="ri-user-settings-line w-5 h-5 mr-3"></i>
+                    <span>Profile</span>
+                </a>
+            </li>
 
-            <li class="mt-8">
-                <div x-data="{ open: false }" class="relative">
-                    <button @click="open = !open" class="flex items-center justify-between w-full p-2 rounded hover:bg-gray-700">
-                        <div class="flex items-center">
-                            <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path>
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                            </svg>
-                            <span>Settings</span>
-                        </div>
-                        <svg class="w-4 h-4 transition-transform duration-200 transform" :class="{ 'rotate-180': open }" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-                        </svg>
-                    </button>
-
-                    <div x-show="open" @click.away="open = false" class="mt-2 ml-8 space-y-2">
-                        <a href="{{ route('profile.edit') }}" class="block p-2 rounded hover:bg-gray-700">Profile</a>
-                        <a href="{{ route('profile.edit') }}" class="block p-2 rounded hover:bg-gray-700">Branches</a>
-                        <a href="{{ route('profile.edit') }}" class="block p-2 rounded hover:bg-gray-700">Notifications</a>
-                    </div>
-                </div>
+            <li>
+                <a href="#" class="flex items-center p-2 rounded hover:bg-gray-700 ">
+                    <i class="ri-settings-2-line w-5 h-5 mr-3"></i>
+                    <span>Settings</span>
+                </a>
             </li>
         </ul>
     </nav>
